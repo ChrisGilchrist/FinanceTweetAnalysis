1749d0299eba1971a35f41c68557965865f349d6
Chris Gilchrist <chris+hackathon@quix.io>
2024-02-08 10:32:20 +0000

    Edited 'main.py'

--- StockTwits/main.py
+++ StockTwits/main.py
@@ -6,7 +6,6 @@ import os
 client = qx.QuixStreamingClient()
 
 # Use Input / Output topics to stream data in or out of your service
-consumer_topic = client.get_topic_consumer(os.environ["input"])
 producer_topic = client.get_topic_producer(os.environ["output"])
 
 # for more samples, please see samples or docs
