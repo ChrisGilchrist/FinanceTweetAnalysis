1b8d5bb1d565301123e55897e455088b1d4be10f
Chris Gilchrist <chris+hackathon@quix.io>
2024-02-08 09:33:27 +0000

    Edited 'main.py'

--- Sentiment Analysis/main.py
+++ Sentiment Analysis/main.py
@@ -1,12 +1,59 @@
-import quixstreams as qx
 import os
+from quixstreams import Application, State
+from quixstreams.models.serializers.quix import JSONDeserializer, JSONSerializer
+from transformers import pipeline
 
-# Quix injects credentials automatically to the client.
-# Alternatively, you can always pass an SDK token manually as an argument.
-client = qx.QuixStreamingClient()
+# import our get_app function to help with building the app for local/Quix deployed code
+from app_factory import get_app
 
-# Use Input / Output topics to stream data in or out of your service
-consumer_topic = client.get_topic_consumer(os.environ["input"])
-producer_topic = client.get_topic_producer(os.environ["output"])
+# import the dotenv module to load environment variables from a file
+from dotenv import load_dotenv
+load_dotenv(override=False)
 
-# for more samples, please see samples or docs
+# get the environment variable value or default to False
+USE_LOCAL_KAFKA=os.getenv("use_local_kafka", False)
+
+# Create an Application.
+app = get_app(use_local_kafka=USE_LOCAL_KAFKA)
+
+# Set the pipeline
+classifier = pipeline("text-classification", model="StephanAkkerman/FinTwitBERT-sentiment")
+
+input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
+output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer())
+
+sdf = app.dataframe(input_topic)
+
+def count_names(row: dict, state: State):
+
+    # get the value from the name column for this row
+    # so we can see if it's in state
+    name = row["symbols"]
+
+    # check state, if the name is already there then retrieve the count
+    # default to 0 if the name wasn't in state
+    name_count = state.get(name, 0)
+
+    # add one to the name count
+    name_count += 1
+
+    # add the name count to the row data
+    row["count"] = name_count
+
+    # store the new count in state
+    state.set(name, name_count)
+
+    # return the updated row so more processing can be done on it
+    return row
+
+# apply the result of the count_names function to the row
+sdf = sdf.apply(count_names, stateful=True)
+
+# print the row with this inline function
+sdf = sdf.update(lambda row: print(row))
+
+# publish the updated row to the output topic
+sdf = sdf.to_topic(output_topic)
+
+if __name__ == "__main__":
+    app.run(sdf)
