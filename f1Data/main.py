18151ce86945c44addf65cb838d72c91ec05c225
Chris Gilchrist <chris+hackathon@quix.io>
2024-02-09 16:24:59 +0000

    Edited 'main.py'

--- f1Data/main.py
+++ f1Data/main.py
@@ -10,9 +10,25 @@ client = qx.QuixStreamingClient()
 consumer_topic = client.get_topic_consumer(os.environ["input"])
 producer_topic = client.get_topic_producer(os.environ["output"])
 
-session = fastf1.get_session(2019, 'Monza', 'Q')
-session.load(telemetry=False, laps=False, weather=False)
-vettel = session.get_driver('VET')
-print(f"Pronto {vettel['FirstName']}?")
+from matplotlib import pyplot as plt
+import fastf1
+import fastf1.plotting
 
-# for more samples, please see samples or docs
+fastf1.plotting.setup_mpl()
+
+session = fastf1.get_session(2019, 'Monza', 'Q')
+
+session.load()
+fast_leclerc = session.laps.pick_driver('LEC').pick_fastest()
+lec_car_data = fast_leclerc.get_car_data()
+t = lec_car_data['Time']
+vCar = lec_car_data['Speed']
+
+# The rest is just plotting
+fig, ax = plt.subplots()
+ax.plot(t, vCar, label='Fast')
+ax.set_xlabel('Time')
+ax.set_ylabel('Speed [Km/h]')
+ax.set_title('Leclerc is')
+ax.legend()
+plt.show()
