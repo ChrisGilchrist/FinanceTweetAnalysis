23df50bc980339b5dfcf9ec4183d2027be1186bd
Chris Gilchrist <chris+hackathon@quix.io>
2024-02-09 16:36:43 +0000

    Edited 'main.py'

--- f1Data/main.py
+++ f1Data/main.py
@@ -9,25 +9,8 @@ client = qx.QuixStreamingClient()
 # Use Input / Output topics to stream data in or out of your service
 consumer_topic = client.get_topic_consumer(os.environ["input"])
 producer_topic = client.get_topic_producer(os.environ["output"])
+session = fastf1.get_session(2021, 7, 'Q')
 
-from matplotlib import pyplot as plt
-import fastf1.plotting
-
-fastf1.plotting.setup_mpl()
-
-session = fastf1.get_session(2019, 'Monza', 'Q')
-
-session.load()
-fast_leclerc = session.laps.pick_driver('LEC').pick_fastest()
-lec_car_data = fast_leclerc.get_car_data()
-t = lec_car_data['Time']
-vCar = lec_car_data['Speed']
-
-# The rest is just plotting
-fig, ax = plt.subplots()
-ax.plot(t, vCar, label='Fast')
-ax.set_xlabel('Time')
-ax.set_ylabel('Speed [Km/h]')
-ax.set_title('Leclerc is')
-ax.legend()
-plt.show()
+session.name
+session.date
+print(session.date)
